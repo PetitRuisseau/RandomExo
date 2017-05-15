9e4a9877d5e40fde8643e618efd777ef581c0a67
Clémence <prtranquille@gmail.com>
2017-05-15 22:19:18 +0200

    random exo while avec jean !

--- jeanvascript/js.js
+++ jeanvascript/js.js
@@ -3,3 +3,31 @@ function test() {
 };
 
 document.querySelector("a").addEventListener("click", test);
+
+//---------------//
+
+/*let i = 0;
+while (i != 4) {
+    i = prompt("2+2?");
+}*/
+
+document.querySelector("form").addEventListener('submit', function(e) {
+    let texte = document.querySelector("#text").value;
+    if (texte != "") {
+        let sub = document.querySelector("#sub");
+        sub.className = "green";
+    } else {
+        let sub = document.querySelector("#sub");
+        sub.className = "red";
+    }
+    document.querySelector("#sub").addEventListener("webkitAnimationEnd", function() {
+        document.querySelector("#sub").className.remove;
+        console.log("ok");
+    });
+    e.preventDefault();
+});
+
+document.querySelector("#sub").addEventListener("webkitAnimationEnd", function() {
+    document.querySelector("#sub").className.remove;
+    console.log("ok");
+});
